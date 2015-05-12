98e2bf5c663c0b7d8a35cff7b6327f3c11e8e75e
Nicolas Berger <nicoberger@gmail.com>
2015-05-11 22:54:18 -0300

    Remove :aot :all

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/guilespi/time-series-storage"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :aot :all
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/java.jdbc "0.3.5"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
