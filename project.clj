94265e9473bff4f49021013ccceae122a883effa
ichramm <jramirez.uy@gmail.com>
2021-08-05 16:11:00 -0300

    Bump version 0.3.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject time-series-storage "0.3.9"
+(defproject time-series-storage "0.3.10"
   :description "Time Series Storage Library"
   :url "https://github.com/guilespi/time-series-storage"
   :license {:name "Eclipse Public License"
